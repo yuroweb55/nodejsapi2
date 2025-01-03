1c0ac2c0e2aec5244e28a54a3e7b1ea5f2441723
yuroweb55 <168056659+yuroweb55@users.noreply.github.com>
2025-01-03 09:12:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const roo = ()=>fetch("https://app.bbp168.com/login-json-check", {
-    "headers": {
-      "accept": "*/*",
-      "accept-language": "th",
-      "content-type": "application/json",
-      "priority": "u=1, i",
-      "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"Windows\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-requested-with": "XMLHttpRequest",
-      "cookie": "ci_session=jm7srs1khe2jh72lric34is7qpim2e53",
-      "Referer": "https://app.bbp168.com/",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "body": "{\"username\":\"095456456456\",\"password\":\"gffhhgfh\"}",
-    "method": "POST"
-  });
+const roo = ()=>{
+    fetch("https://app.bbp168.com/login-json-check", {
+        "headers": {
+          "accept": "*/*",
+          "accept-language": "th",
+          "content-type": "application/json",
+          "priority": "u=1, i",
+          "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-requested-with": "XMLHttpRequest",
+          "cookie": "ci_session=jm7srs1khe2jh72lric34is7qpim2e53",
+          "Referer": "https://app.bbp168.com/",
+          "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": "{\"username\":\"095456456456\",\"password\":\"gffhhgfh\"}",
+        "method": "POST"
+    }).then(response => {
+      
+    }).catch(error => {
+          
+    });
+}
 
 
   setInterval(roo,1);
